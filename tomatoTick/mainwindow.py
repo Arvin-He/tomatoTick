0dc8ad9fbd696c5c5effa3155eb34e5995eb796e
Arvin-He <510205617@qq.com>
2017-03-27 22:13:41 +0800

    center the window

--- tomatoTick/mainwindow.py
+++ tomatoTick/mainwindow.py
@@ -1,7 +1,8 @@
 from PyQt5 import QtCore
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QDesktopWidget
+from PyQt5.QtCore import QRect
 import sys
 
 class TomatoTickWindow(QMainWindow):
@@ -9,10 +10,14 @@ class TomatoTickWindow(QMainWindow):
   def __init__(self):
     super(TomatoTickWindow, self).__init__()
     self.initUI()
-    self.show()
+    
     
   def initUI(self):
-    pass
+    self.setWindowTitle("TomatoClock")
+    screen = QDesktopWidget().screenGeometry()
+    self.setGeometry(QRect((screen.width()-self.geometry().width())/2, 
+                           (screen.height()-self.geometry().height())/2, 600, 500))
+    self.show()
 
   
 if __name__ == "__main__":
